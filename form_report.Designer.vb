40ca1ca7ed9303d59132b8de5d8b4d096ccde1c9
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-11-15 11:51:22 +0000

    ui design

---
+++ form_report.Designer.vb
@@ -0,0 +1,333 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form_crystalReports
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lbl_studentInfo = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.lbl_fullName = New System.Windows.Forms.Label()
+        Me.lbl_gender = New System.Windows.Forms.Label()
+        Me.lbl_unit = New System.Windows.Forms.Label()
+        Me.lbl_subject = New System.Windows.Forms.Label()
+        Me.lbl_address = New System.Windows.Forms.Label()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_loadData = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btn_search = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_countRows = New System.Windows.Forms.Label()
+        Me.lbl_countRowNum = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.TextBox4)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.TextBox6)
+        Me.Panel1.Controls.Add(Me.lbl_address)
+        Me.Panel1.Controls.Add(Me.lbl_subject)
+        Me.Panel1.Controls.Add(Me.lbl_unit)
+        Me.Panel1.Controls.Add(Me.lbl_gender)
+        Me.Panel1.Controls.Add(Me.lbl_fullName)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Location = New System.Drawing.Point(42, 53)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(613, 196)
+        Me.Panel1.TabIndex = 0
+        '
+        'lbl_studentInfo
+        '
+        Me.lbl_studentInfo.AutoSize = True
+        Me.lbl_studentInfo.Font = New System.Drawing.Font("Segoe UI Semibold", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_studentInfo.Location = New System.Drawing.Point(177, 9)
+        Me.lbl_studentInfo.Name = "lbl_studentInfo"
+        Me.lbl_studentInfo.Size = New System.Drawing.Size(327, 37)
+        Me.lbl_studentInfo.TabIndex = 1
+        Me.lbl_studentInfo.Text = "STUDENT INFORMATION" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(319, 28)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(260, 25)
+        Me.TextBox3.TabIndex = 2
+        '
+        'lbl_fullName
+        '
+        Me.lbl_fullName.AutoSize = True
+        Me.lbl_fullName.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_fullName.Location = New System.Drawing.Point(19, 8)
+        Me.lbl_fullName.Name = "lbl_fullName"
+        Me.lbl_fullName.Size = New System.Drawing.Size(79, 20)
+        Me.lbl_fullName.TabIndex = 5
+        Me.lbl_fullName.Text = "Full Name"
+        '
+        'lbl_gender
+        '
+        Me.lbl_gender.AutoSize = True
+        Me.lbl_gender.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_gender.Location = New System.Drawing.Point(19, 71)
+        Me.lbl_gender.Name = "lbl_gender"
+        Me.lbl_gender.Size = New System.Drawing.Size(59, 20)
+        Me.lbl_gender.TabIndex = 6
+        Me.lbl_gender.Text = "Gender"
+        '
+        'lbl_unit
+        '
+        Me.lbl_unit.AutoSize = True
+        Me.lbl_unit.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_unit.Location = New System.Drawing.Point(19, 136)
+        Me.lbl_unit.Name = "lbl_unit"
+        Me.lbl_unit.Size = New System.Drawing.Size(38, 20)
+        Me.lbl_unit.TabIndex = 7
+        Me.lbl_unit.Text = "Unit"
+        '
+        'lbl_subject
+        '
+        Me.lbl_subject.AutoSize = True
+        Me.lbl_subject.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_subject.Location = New System.Drawing.Point(318, 8)
+        Me.lbl_subject.Name = "lbl_subject"
+        Me.lbl_subject.Size = New System.Drawing.Size(59, 20)
+        Me.lbl_subject.TabIndex = 8
+        Me.lbl_subject.Text = "Subject"
+        '
+        'lbl_address
+        '
+        Me.lbl_address.AutoSize = True
+        Me.lbl_address.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_address.Location = New System.Drawing.Point(316, 71)
+        Me.lbl_address.Name = "lbl_address"
+        Me.lbl_address.Size = New System.Drawing.Size(63, 20)
+        Me.lbl_address.TabIndex = 9
+        Me.lbl_address.Text = "Address"
+        '
+        'btn_save
+        '
+        Me.btn_save.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn_save.FlatAppearance.BorderSize = 0
+        Me.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_save.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_save.ForeColor = System.Drawing.Color.White
+        Me.btn_save.Location = New System.Drawing.Point(255, 258)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(88, 25)
+        Me.btn_save.TabIndex = 2
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = False
+        '
+        'btn_update
+        '
+        Me.btn_update.BackColor = System.Drawing.Color.Blue
+        Me.btn_update.FlatAppearance.BorderSize = 0
+        Me.btn_update.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_update.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_update.ForeColor = System.Drawing.Color.White
+        Me.btn_update.Location = New System.Drawing.Point(152, 258)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(85, 26)
+        Me.btn_update.TabIndex = 3
+        Me.btn_update.Text = "Update"
+        Me.btn_update.UseVisualStyleBackColor = False
+        '
+        'btn_delete
+        '
+        Me.btn_delete.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn_delete.FlatAppearance.BorderSize = 0
+        Me.btn_delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_delete.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_delete.ForeColor = System.Drawing.Color.White
+        Me.btn_delete.Location = New System.Drawing.Point(361, 258)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(88, 26)
+        Me.btn_delete.TabIndex = 4
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.UseVisualStyleBackColor = False
+        '
+        'btn_loadData
+        '
+        Me.btn_loadData.BackColor = System.Drawing.Color.Blue
+        Me.btn_loadData.FlatAppearance.BorderSize = 0
+        Me.btn_loadData.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_loadData.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_loadData.ForeColor = System.Drawing.Color.White
+        Me.btn_loadData.Location = New System.Drawing.Point(468, 258)
+        Me.btn_loadData.Name = "btn_loadData"
+        Me.btn_loadData.Size = New System.Drawing.Size(88, 27)
+        Me.btn_loadData.TabIndex = 5
+        Me.btn_loadData.Text = "Load Data"
+        Me.btn_loadData.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(42, 359)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(613, 186)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'btn_search
+        '
+        Me.btn_search.BackColor = System.Drawing.Color.Blue
+        Me.btn_search.FlatAppearance.BorderSize = 0
+        Me.btn_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_search.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_search.ForeColor = System.Drawing.Color.White
+        Me.btn_search.Location = New System.Drawing.Point(574, 327)
+        Me.btn_search.Name = "btn_search"
+        Me.btn_search.Size = New System.Drawing.Size(81, 26)
+        Me.btn_search.TabIndex = 8
+        Me.btn_search.Text = "Search"
+        Me.btn_search.UseVisualStyleBackColor = False
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(22, 28)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(260, 25)
+        Me.TextBox6.TabIndex = 10
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(22, 91)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(260, 25)
+        Me.TextBox1.TabIndex = 11
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(319, 91)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(260, 25)
+        Me.TextBox2.TabIndex = 12
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(22, 156)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(147, 25)
+        Me.TextBox4.TabIndex = 13
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(463, 329)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox5.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(373, 329)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 20)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "ID Number"
+        '
+        'lbl_countRows
+        '
+        Me.lbl_countRows.AutoSize = True
+        Me.lbl_countRows.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_countRows.Location = New System.Drawing.Point(55, 332)
+        Me.lbl_countRows.Name = "lbl_countRows"
+        Me.lbl_countRows.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_countRows.TabIndex = 11
+        Me.lbl_countRows.Text = "Count row(s):"
+        '
+        'lbl_countRowNum
+        '
+        Me.lbl_countRowNum.AutoSize = True
+        Me.lbl_countRowNum.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_countRowNum.Location = New System.Drawing.Point(157, 333)
+        Me.lbl_countRowNum.Name = "lbl_countRowNum"
+        Me.lbl_countRowNum.Size = New System.Drawing.Size(17, 20)
+        Me.lbl_countRowNum.TabIndex = 12
+        Me.lbl_countRowNum.Text = "0"
+        '
+        'form_crystalReports
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(698, 552)
+        Me.Controls.Add(Me.lbl_countRowNum)
+        Me.Controls.Add(Me.lbl_countRows)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.btn_search)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btn_loadData)
+        Me.Controls.Add(Me.btn_delete)
+        Me.Controls.Add(Me.btn_update)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.lbl_studentInfo)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "form_crystalReports"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Crystal Reports"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lbl_studentInfo As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents lbl_address As Label
+    Friend WithEvents lbl_subject As Label
+    Friend WithEvents lbl_unit As Label
+    Friend WithEvents lbl_gender As Label
+    Friend WithEvents lbl_fullName As Label
+    Friend WithEvents btn_save As Button
+    Friend WithEvents btn_update As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_loadData As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btn_search As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl_countRows As Label
+    Friend WithEvents lbl_countRowNum As Label
+End Class
---
+++ form_report.vb
@@ -0,0 +1,5 @@
+﻿Public Class form_crystalReports
+    Private Sub form_crystalReports_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
